8af2aa860011ee1535abad3e9090634596fbf447
YannickSpoerl <spoerlyannick@gmail.com>
2020-02-02 17:11:27 +0100

    Add Vuetify

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import vuetify from '@/plugins/vuetify' // path to vuetify export
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
+  vuetify,
   el: '#app',
   components: { App },
   template: '<App/>'
